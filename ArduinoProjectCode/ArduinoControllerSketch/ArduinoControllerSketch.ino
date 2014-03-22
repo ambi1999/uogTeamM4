8a34544c5c07df1bde3d8238ae2394bc08102d03
Matt <m4ttmo@gmail.com>
2014-03-22 15:28:09 +0000

    Update ArduinoControllerSketch.ino
    This should be the final one, Test on monday but it should work. The for loops split up the run so I can tell where an issue is quicker as well as performing quicker as the LEDs blink together now. I didn't comment it but can do if necessary.

--- ArduinoProjectCode/ArduinoControllerSketch/ArduinoControllerSketch.ino
+++ ArduinoProjectCode/ArduinoControllerSketch/ArduinoControllerSketch.ino
@@ -1,31 +1,72 @@
-int incomingByte = 0;   // for incoming serial data
- 
+int Leds[3] = {2, 3, 4};
+int Sensors[3] = {A0, A1, A2};
+int RGB[3] = {0, 0, 0};
+int White[3] = {0, 0, 0};
+int Black[3] = {0, 0, 0};
+boolean Calibrated = false;
 void setup(){
+ pinMode(Leds[1,2,3],OUTPUT);
   Serial.begin(9600);
-  pinMode(12, OUTPUT);     
-  pinMode(8, OUTPUT);     
-}
- 
- 
+  }
+  
 void loop(){
-  // send data only when you receive data:
-        if (Serial.available() > 0) {
-                //digitalWrite(12, HIGH);   
-                // read the incoming byte:
-                incomingByte = Serial.read()-'0';
-                //incomingByte = Serial.read();
-                Serial.println("I received "+ incomingByte);
- 
-                // say what you got:
-                if(incomingByte==1){
-                  digitalWrite(12, HIGH);
-                }else if(incomingByte==2){
-                  digitalWrite(12, LOW);
-                }else if(incomingByte==3){
-                  digitalWrite(8, HIGH);
-                }else if(incomingByte==4){
-                  digitalWrite(8, LOW);
-                }
-        }
+  if(Calibrated == false){
+    Calibrate();
+  }  
+Scan();
 }
+void Scan(){
+ Serial.println("Scan Colour");
+ for(int i=0; i>2; i++){
+ digitalWrite(Leds[i], HIGH); 
+ }
+ delay(1000);
+for(int i=0; i>2; i++){
+ digitalWrite(Leds[i], LOW); 
+ }
+for(int i=0; i>2; i++){
+ RGB[i] = analogRead(Sensors[i]);
+ }
+delay(1000);
+ for(int i=0; i>2; i++){
+ RGB[i]= ((RGB[i]*(White[i]-Black[i]))*255);
+ }
+ Serial.print("R = ");
+ Serial.println(RGB[1]);
+ Serial.print("G = ");
+ Serial.println(RGB[2]);
+ Serial.print("B = ");
+ Serial.println(RGB[3]); 
+ Serial.println("Colour scanned");
+  }
+void Calibrate(){
+ Serial.println("Calibrate White");
+  for(int i=0; i>2; i++){
+ digitalWrite(Leds[i], HIGH); 
+ }
+ delay(1000);
+for(int i=0; i>2; i++){
+ digitalWrite(Leds[i], LOW); 
+ }
+ for(int i=0; i>2; i++){
+ White[i] = analogRead(Sensors[i]);
+ }
+ delay(5000);
+ Serial.println("Calibrate Black");
+ for(int i=0; i>2; i++){
+ digitalWrite(Leds[i], HIGH); 
+ }
+ delay(1000);
+for(int i=0; i>2; i++){
+ digitalWrite(Leds[i], LOW); 
+ }
+ for(int i =0; i>2; i++){
+ Black[i] = analogRead(i);
+  }
+ Calibrated = true;
+ loop();
+}
+  
+  
+  
 
