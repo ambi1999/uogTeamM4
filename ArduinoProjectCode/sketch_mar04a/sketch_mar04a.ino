a695b560439e0ed107e4bed0a9b68a1515d589d9
Matt <m4ttmo@gmail.com>
2014-03-26 20:58:26 +0000

    Still won't Scan!!1
    :(

--- ArduinoProjectCode/sketch_mar04a/sketch_mar04a.ino
+++ ArduinoProjectCode/sketch_mar04a/sketch_mar04a.ino
@@ -4,6 +4,8 @@ int Sensors[3] = {A2, A1, A0}; //sensors used in the scanning
 int RGB[3] = {0, 0, 0}; //Array stores the sensor readings for the colour
 int White[3] = {0, 0, 0};// array stores the white balance reading 
 int Black[3] = {0, 0, 0}; // Stores the Black balance readings
+int WhiteHolder = 1023;
+int BlackHolder =1023;
 boolean Calibrated = false;
 void setup(){
  //setting all the led pins as outputs
@@ -19,41 +21,43 @@ void setup(){
   }
   
 void loop(){
+  
   delay(5000); //Delay allows for user to prepare the correct cards 
   if(Calibrated == false){ //checks if the system has been  calibrated
     Calibrate(); //If not sends to Calibrate function
   }  
  Scan();//if has sends to scan function
- delay(60000); //Longer delay allows users to see result of scan before scanning starts again
-}
- 
+ delay(10000); //Longer delay allows users to see result of scan before scanning starts again
 
+}
   //Scan Function scans and converts the RGB values
 void Scan(){
  Serial.println("Scan Colour"); //Tells user to scan Colour
  digitalWrite(Indicator[2],HIGH); //Sets the indication pin to say scanning has started
- for(int i=0; i<3; i++){ //As the Arrays have 3 sets of values the for loop starts at 0 and +1 every iteration until  and including 2
- digitalWrite(Leds[i], HIGH); //turn on scanning pins
- }
- delay(1000);//wait 1 sec
+//as the pins are different brightnessess this evens them out so the reading is correct
+ analogWrite(Leds[0], 400);
+ /*
+ The LEDs all have a different brightness when using digitalWrite(Leds[i], HIGH).
+ This then interferes with the colour detection so by using analogWrite(leds[0], Value).
+ We are able to equal out the brightness of the leds
+ */
+ analogWrite(Leds[1], 1500); //turn on scanning pins
+ analogWrite(Leds[2], 20); //turn on scanning pins
+  delay(1000);//wait 1 sec
 for(int i=0; i<3; i++){
- digitalWrite(Leds[i], LOW); //Turn Off scanning Pins 
- }
-for(int i=0; i<3; i++){
- RGB[i] = analogRead(Sensors[i]); //Read sensor values
+  RGB[i] = analogRead(Sensors[i]); //Read sensor values
+  digitalWrite(Leds[i], LOW); //Turn Off scanning Pins
  }
 delay(1000); //delay allows for system and user to re-sync 
-for(int i=0; i<3; i++){
-   int Boundary = White[i] -Black[i] //Setting the black value as the 0 point by taking it off the white to move the scale to start at 0
-   int range = Boundary/255 //using Boundary as the maximum value of reflection we divide by 255 to gain the ratio value
-   RGB[i] = (RGB[i] / range); //Dividing by the range then gives use the value for the colour inside the RGB scale
-   if (RGB[i]>255){ //If the RGB Value is outside 255 the if sets it back to 255
-   RGB[i]=255;
-   }
-   if (RGB[i]<0){ // If the RGB value is below 0 the if sets it back to 0
-   RGB[i]=0;
-   }
-  }
+ for(int i=0; i<3; i++){
+ int Range = White[i] - Black[i]; //This moves the scale from Black[i] to white[i] to have a base 0 with scale.
+ RGB[i] = ((RGB[i]-Black[i])/(Range)*255);
+ /*
+ By taking the Black away from the colour we adjust for the white balance. 
+ By timsing the range by 255 we bring it on the Scale.
+ Dividing the 2 brings the RGB[] scanned value into the range.
+ */
+ }
  Serial.print("R = ");
  Serial.println(RGB[0]); //Prints the Array Values. Red
  Serial.print("G = ");
@@ -67,35 +71,35 @@ for(int i=0; i<3; i++){
 void Calibrate(){
  Serial.println("Calibrate White");
  digitalWrite(Indicator[0],HIGH); //Tells the user which stage the processes is at
- delay(1000) //Delay for user to set white card
-  for(int i=0; i<3; i++){
- digitalWrite(Leds[i], HIGH); //Turns the LEDs
- }
+ delay(1000); //Delay for user to set white card
+ //as the pins are different brightnessess this evens them out so the reading is correct
+ analogWrite(Leds[0], 400); //turn on scanning pins
+ analogWrite(Leds[1], 1500); //turn on scanning pins
+ analogWrite(Leds[2], 20); //turn on scanning pins
  delay(1000);
 for(int i=0; i<3; i++){
- digitalWrite(Leds[i], LOW);//Turns Leds off  
  White[i] = analogRead(Sensors[i]); //Reads the sensor values and stores in the Array
- }
- 
+ digitalWrite(Leds[i], LOW);//Turns Leds off 
+}
  for(int i=0; i<3; i++){
  Serial.println(White[i]); //Prints only for testing 
  }
  delay(5000); //Allows user to place black card
  Serial.println("Calibrate Black");
  digitalWrite(Indicator[1],HIGH);
- delay(1000) //Delay to allow user to set black card
- for(int i=0; i<3; i++){
- digitalWrite(Leds[i], HIGH); //LEDs on
- }
+ delay(1000); //Delay to allow user to set black card
+//as the pins are different brightnessess this evens them out so the reading is correct
+ analogWrite(Leds[0], 400); //turn on scanning pins
+ analogWrite(Leds[1], 1500); //turn on scanning pins
+ analogWrite(Leds[2], 20); //turn on scanning pins
  delay(1000);
 for(int i=0; i<3; i++){
- digitalWrite(Leds[i], LOW);  //LEDs off
- Black[i] = analogRead(Sensors[i]); //Reads the sensor values and stores them into an array
- }
+  Black[i] = analogRead(Sensors[i]); //Reads the sensor values and stores them into an array
+  digitalWrite(Leds[i], LOW);  //LEDs off
+   }
   for(int i=0; i<3; i++){
- Serial.println(Black[i]); //Testing Purposes
+ Serial.println(Black[i]); //Testing Purposes. prints the black value so the algorithm can be hand implimented 
   }
  delay(10000);
  Calibrated = true; //sets calibrated to true to skip the calibration process for the rest of the session
 }
-//Comment 
